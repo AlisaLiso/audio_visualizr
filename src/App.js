2ccf5c8368f1028063649f4ebbeeaaeee0498a97
alisaliso <alisaliso.com@gmail.com>
2021-01-21 00:53:59 +0300

    Add 2D vuew of the visualizer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useState } from 'react';
 import Cube from './Cube';
+import Rectangle from './Rectangle';
 import './App.scss';
 
 function App() {
+  const [isBox3D, setIsBox3D] = useState(false);
+  const [isBox2D, setIsBox2D] = useState(true);
+
+  const enable3DVuew = () => {
+    if (isBox3D) return;
+
+    setIsBox2D(false);
+    setIsBox3D(true);
+  }
+
+  const enable2DVuew = () => {
+    if (isBox2D) return;
+
+    setIsBox3D(false);
+    setIsBox2D(true);
+  }
+
   return (
     <div className="container">
       <div className="wrapper">
@@ -12,15 +31,21 @@ function App() {
       <div className="wrapper">
         <div className="radio-btn">
           <p>
-            <input type="radio" id="test1" name="radio-group" checked />
-            <label for="test1">3D</label>
+            <input type="radio" id="test1" name="radio-group" />
+            <label onClick={enable3DVuew} for="test1">3D</label>
           </p>
           <p>
-            <input type="radio" id="test2" name="radio-group" />
-            <label for="test2">2D</label>
+            <input type="radio" id="test2" name="radio-group" checked />
+            <label onClick={enable2DVuew} for="test2">2D</label>
           </p>
         </div>
-        <Cube />
+        {isBox3D &&
+          <Cube />
+        }
+
+        {isBox2D &&
+          <Rectangle />
+        }
       </div>
     </div>
   );
---
+++ src/Rectangle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Rect.scss';
+
+export default function Rectangle() {
+  return (
+    <div className="rect" style={{
+      backgroundImage: 'linear-gradient(to bottom, transparent 0%, transparent 60%, #A0BFFB 60%, #A0BFFB 75%, #79a7ff 75%)'
+    }}>
+    </div>
+  )
+}
