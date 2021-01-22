8c9eb2a61ea6fa7279e5c2b0f1c289014e00fd73
alisaliso <alisaliso.com@gmail.com>
2021-01-22 21:09:45 +0300

    Get audio context on mic click

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Rect.scss';
 
-export default function Rectangle() {
+export default function Rectangle({ isMicActive, setIsMicActive, onRectClick }) {
+  const [highestPoint, setHighesPoint] = useState(60);
+  const [lowestPoint, setLowestPoint] = useState(75);
+
+  const toggleMic = () => {
+    setIsMicActive((prev) => !prev);
+    onRectClick();
+  }
+
   return (
-    <div className="rect" style={{
-      backgroundImage: 'linear-gradient(to bottom, transparent 0%, transparent 60%, #A0BFFB 60%, #A0BFFB 75%, #79a7ff 75%)'
+    <div className="rect" onClick={toggleMic} style={{
+      backgroundImage: `linear-gradient(to bottom, transparent 0%, transparent ${highestPoint}%, var(--boxSubFillColor) ${highestPoint}%, var(--boxSubFillColor) ${lowestPoint}%, var(--boxFillColor) ${lowestPoint}%)`
     }}>
+      <img className={`${!isMicActive && "rect__icon-hidden"}`}
+        src={window.location.origin + '/mic.svg'} alt="Turn mic off" />
+      <img className={`${isMicActive && "rect__icon-hidden"}`}
+        src={window.location.origin + '/mic_off.svg'} alt="Turn mic on" />
     </div>
   )
 }
